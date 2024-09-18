df8208a7fc4838999873bdca0daca2735c075998
kamlakanwar <kamlakanwar923@gmail.com>
2024-09-18 23:12:23 +0530

    wheather commit

---
+++ myscript.js
@@ -0,0 +1,46 @@
+const apiKey = "c43489316f17e5df06973e781cb07f8a";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather() {
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+
+    if(response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+    else{
+    var data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/hr";
+
+    if(data.weather[0].main == "Clouds" ) {
+        weatherIcon.src = "clouds.png"
+    }
+    else if(data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "drizzle.png"
+    }
+    else if(data.weather[0].main == "Clear") {
+        weatherIcon.src = "clear.png"
+    }
+    else if(data.weather[0].main == "Mist") {
+        weatherIcon.src = "mist.png"
+    }
+    else if(data.weather[0].main == "Snow") {
+        weatherIcon.src = "snow.png"
+    }
+    
+    document.querySelector(".weather").style.display ="block";
+    document.querySelector(".weather").style.display ="none";
+    
+    }
+}
+
+searchBtn.addEventListner("click", ()=> {
+    checkWeather(searchBox.value);
+})
